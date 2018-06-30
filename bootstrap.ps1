48b6c1b4c0815633cd911ec256c2198d62ac508b
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-06-30 16:33:50 +0530

    foo

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -26,7 +26,7 @@ $env:Path = $newSystemPath + ";" + [System.Environment]::GetEnvironmentVariable(
 npm config set msvs_version 2015
 git clone https://github.com/gocd/gocd --depth 1 C:\\gocd
 cd C:\\gocd
-./gradlew.bat compileAll yarnInstall prepareJRuby --no-daemon --no-build-cache
+./gradlew.bat compileAll yarnInstall prepareJRuby --no-build-cache
 tasklist
 taskkill /F /IM java.exe
 echo sleeping
