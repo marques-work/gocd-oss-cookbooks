73a2d7ec4956cabd1d34812c478f136941cfecc9
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2018-06-30 17:44:59 +0530

    foo

--- bootstrap2.ps1
+++ bootstrap2.ps1
@@ -4,6 +4,7 @@ git clone https://github.com/gocd/gocd --depth 1 C:\\gocd --quiet
 cd C:\\gocd
 tasklist
 ./gradlew.bat compileAll yarnInstall prepareJRuby --no-build-cache --quiet
+git clean -ffddx
 tasklist
 taskkill /F /IM java.exe
 echo sleeping
